c34c5b6fc941c6cd6f66241bf4675fe2100f6a78
fironkanon-gif <fironkanon@gmail.com>
2026-09-07 18:14:33 +0300

    Create __init__.py

---
+++ GothicOCR_New/p4a_recipes/freetype/__init__.py
@@ -0,0 +1,8 @@
+from pythonforandroid.recipe import Recipe
+
+class FreeTypeRecipe(Recipe):
+    version = '2.14.1'
+    url = 'https://downloads.sourceforge.net/project/freetype/freetype2/{version}/freetype-{version}.tar.gz'
+    built_libraries = {'libfreetype.so': 'objs/.libs'}
+
+recipe = FreeTypeRecipe()
